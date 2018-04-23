b6a6c9191eb721d08c1b8cdd516e10f36b667ad5
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-04-23 12:35:54 -0700

    Fixed setup.sh script

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sed -x
+set -x
 
 SED_DB_DATABASE=${DB_DATABASE:-imsdb} 
 SED_DB_HOST=${DB_HOST:-db-host} 
@@ -8,9 +8,9 @@ SED_DB_USER=${DB_USER:-aceims}
 SED_DB_PASSWORD=${DB_PASSWORD:-ace}
 SED_SMTP_HOST=${SMTP_HOST:-smtp.gmail.com:587}
 SED_SMTP_TLS=${SMTP_TLS:-true}
-SED_SMTP_USER=${SMTP_USER:-dockertestfilesender}
 SED_SMTP_FROM=${SMTP_FROM:-dockertestfilesender@gmail.com}
-SED_SMTP_PSWD=${SMTP_PASSWORD:-password=\"thisisalongpassword\"}
+SED_SMTP_USER=${SMTP_USER:-dockertestfilesender}
+SED_SMTP_PASSWORD=${SMTP_PASSWORD:-password=\"thisisalongpassword\"}
 
 cat /opt/ace-ims/opt.payara5.glassfish.domains.domain1.config.domain.xml | \
 sed -e "s/__DB_USER__/$SED_DB_USER/g" \
@@ -20,9 +20,9 @@ sed -e "s/__DB_USER__/$SED_DB_USER/g" \
     -e "s/__DB_PORT__/$SED_DB_PORT/g" \
     -e "s/__SMTP_HOST__/$SED_SMTP_HOST/g" \
     -e "s/__SMTP_TLS__/$SED_SMTP_TLS/g" \
-    -e "s/__SMTP_USER__/$SED_SMTP_USER/g" \
-    -e "s/__SMTP_PSWD__/$SED_SMTP_PSWD/g" \
     -e "s/__SMTP_FROM__/$SED_SMTP_FROM/g" \
+    -e "s/__SMTP_USER__/$SED_SMTP_USER/g" \
+    -e "s/__SMTP_PASSWORD__/$SED_SMTP_PASSWORD/g" \
 > ${PAYARA_PATH}/glassfish/domains/domain1/config/domain.xml
 
 if [ "xx${BOOTSTRAP_SLEEP}" != "xx" ]; then
